20950b8970f34a50119e97dc9dc5c4eced2e3471
silverain <ekdldkaa@naver.com>
2025-03-17 15:28:31 +0900

    #14 feat : set mybatics mapper

--- src/main/java/com/example/demo/user/domain/User.java
+++ src/main/java/com/example/demo/user/domain/User.java
@@ -1,7 +1,5 @@
 package com.example.demo.user.domain;
 
-import jakarta.persistence.*;
-import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,18 +8,11 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@Entity
-@Table(name = "\"user\"")
 public class User {
-
-    @Id
-    @Column(name = "social_id", nullable = false, unique = true)
     private String socialId;
-
-    @Column(name = "name", nullable = false)
     private String name;
 
-    @Builder //객체 생성
+    @Builder
     public User(String socialId, String name) {
         this.socialId = socialId;
         this.name = name;
