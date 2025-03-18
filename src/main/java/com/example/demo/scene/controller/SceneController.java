d0905c2b694588c95fad6fe051e129a04d1dee3e
sihyuuun <leesh3597@gmail.com>
2025-03-18 10:34:34 +0900

    #11 feat: add @Operation annotations to SceneController

--- src/main/java/com/example/demo/scene/controller/SceneController.java
+++ src/main/java/com/example/demo/scene/controller/SceneController.java
@@ -5,6 +5,7 @@ import com.example.demo.scene.dto.SceneCreateRequest;
 import com.example.demo.scene.dto.SceneUpdateRequest;
 import com.example.demo.scene.dto.SceneUpdateVisibilityRequest;
 import com.example.demo.scene.service.SceneService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,34 +15,35 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/scenes")
-@Tag(name="Scene", description="scene 테이블")
+@Tag(name="Scene", description="Scene 테이블 관련 API")
 public class SceneController {
 
     private final SceneService sceneService;
 
-    // SCENE 생성
+    @Operation(summary = "Scene 생성", description = "사용자의 socialId, 위도, 경도를 받아 Scene을 생성")
     @PostMapping
     public ResponseEntity<Scene> createScene(@RequestBody SceneCreateRequest request) {
         Scene scene = sceneService.createScene(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(scene);
     }
 
-    // SCENE 수정 (테마 수정)
+    @Operation(summary = "Scene 수정 (테마 수정)", description = "URL에서 sceneId를 받고 수정할 테마 정보를 RequestBody에서 받아 Scene 테마 수정")
     @PutMapping("/{sceneId}/theme")
     public ResponseEntity<Scene> updateScene(@PathVariable Long sceneId, @RequestBody SceneUpdateRequest request) {
         Scene scene = sceneService.updateScene(sceneId, request);
         return ResponseEntity.ok(scene);
     }
 
+    @Operation(summary = "Scene (메시지) 공개 상태 수정", description = "URL에서 sceneId를 받고 수정할 공개 상태를 RequestBody에서 받아 Scene의 메시지 공개 여부를 수정")
     @PutMapping("/{sceneId}/visibility")
     public ResponseEntity<Scene> updateVisibility(
             @PathVariable Long sceneId,
             @RequestBody SceneUpdateVisibilityRequest request) {
         Scene updatedScene = sceneService.updateVisibility(sceneId, request);
-        return ResponseEntity.ok(updatedScene); // 200 OK로 수정된 Scene 반환
+        return ResponseEntity.ok(updatedScene);
     }
 
-    // SCENE 조회
+    @Operation(summary = "Scene 조회", description = "URL에서 sceneId를 받아 해당 Scene 정보를 조회")
     @GetMapping("/{sceneId}")
     public ResponseEntity<Scene> getScene(@PathVariable Long sceneId) {
         Scene scene = sceneService.getScene(sceneId);
