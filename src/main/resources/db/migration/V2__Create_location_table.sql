c02bac3c2902125e3ef9f8f48918038372304a8d
pillow12360 <ehdcks1256@naver.com>
2025-03-19 17:39:13 +0900

    #develop fix: 수정된 데이터베이스 기준 flyway 스키마 변경

--- src/main/resources/db/migration/V2__Create_location_table.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS location CASCADE;
-
-CREATE TABLE location (
-                          location_id BIGINT AUTO_INCREMENT NOT NULL,
-                          latitude DOUBLE NOT NULL,
-                          longitude DOUBLE NOT NULL,
-                          PRIMARY KEY (location_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
