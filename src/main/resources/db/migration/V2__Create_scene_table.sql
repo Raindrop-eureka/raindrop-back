4e5e3db0853477d1af7bcaf7e5c0eb55a3ba4c5e
pillow12360 <ehdcks1256@naver.com>
2025-03-25 16:43:30 +0900

    #main fix: 마이그레이션 스키마 수정

--- src/main/resources/db/migration/V2__Create_scene_table.sql
+++ src/main/resources/db/migration/V2__Create_scene_table.sql
@@ -1,10 +1,10 @@
 DROP TABLE IF EXISTS scene CASCADE;
 
 CREATE TABLE scene (
-                       scene_id BIGINT AUTO_INCREMENT NOT NULL,
-                       social_id VARCHAR(255) NOT NULL,
-                       theme VARCHAR(255) NOT NULL,
-                       is_visible BOOLEAN NOT NULL DEFAULT FALSE,
-                       PRIMARY KEY (scene_id),
-                       FOREIGN KEY (social_id) REFERENCES `user`(social_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    scene_id BIGINT AUTO_INCREMENT NOT NULL,
+    social_id VARCHAR(255) NOT NULL,
+    theme VARCHAR(255) NOT NULL,
+    is_visible BOOLEAN NOT NULL DEFAULT FALSE,
+    PRIMARY KEY (scene_id),
+    FOREIGN KEY (social_id) REFERENCES user(social_id)
+);
--- src/main/resources/db/migration/V3__Create_message_table.sql
+++ src/main/resources/db/migration/V3__Create_message_table.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS message CASCADE;
 
 CREATE TABLE message (
-                         message_id BIGINT AUTO_INCREMENT NOT NULL,
-                         scene_id BIGINT NOT NULL,
-                         nickname VARCHAR(255) NOT NULL,
-                         content TEXT NOT NULL,
-                         created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                         PRIMARY KEY (message_id),
-                         FOREIGN KEY (scene_id) REFERENCES scene(scene_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    message_id BIGINT AUTO_INCREMENT NOT NULL,
+    scene_id BIGINT NOT NULL,
+    nickname VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (message_id),
+    FOREIGN KEY (scene_id) REFERENCES scene(scene_id)
+);
