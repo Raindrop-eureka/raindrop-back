4350c3cf36eae890d070c606d466028d38e60cc1
silverain <ekdldkaa@naver.com>
2025-05-09 17:01:58 +0900

    hotfix : cors해결을 위한 fe 도메인 추가

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 적용
-                .allowedOrigins("http://localhost:5173") // React 개발 서버 주소
+                .allowedOrigins("http://localhost:5173", "https://raindrop-front.vercel.app") // React 개발 서버 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true) // 인증 정보 허용 (쿠키, 인증 헤더)
